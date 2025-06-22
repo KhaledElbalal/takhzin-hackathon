f91bc54f4be173fa6344d64f3a5cafd427f65887
Khaled Elbalal <KhaledElbalal@gmail.com>
2025-06-22 08:39:25 +0300

    Ensure pallets auto sync with warehouse objects

--- backend/warehouse/apps.py
+++ backend/warehouse/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class WarehouseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'warehouse'
+
+    def ready(self):
+        # Import signal handlers to ensure pallet consistency
+        from . import signals  # noqa: F401
